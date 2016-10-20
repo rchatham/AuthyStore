03c0c78a98131377439df6894df4d64bea8c917a
Reid Chatham <reid.chatham@gmail.com>
2016-10-19 17:09:48 -0700

    initial commit

---
+++ Sources/AuthyStore.swift
@@ -0,0 +1,89 @@
+public struct AuthyStore {
+    
+    public let key : String
+    
+    public init(key: String) {
+        self.key = key
+    }
+    
+    public func requestSMS(countryCode: Int, phoneNumber: String, completionHandler: @escaping (Bool)->Void) {
+        
+        print("request SMS")
+        
+        let requestString = "https://api.authy.com/protected/json/phones/verification/start"
+        
+        var request = URLRequest(url: URL(string: requestString)!)
+        request.httpMethod = "POST"
+        request.addValue("application/json;charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.addValue("278", forHTTPHeaderField: "Content-Length")
+        
+        let json: [String:Any] = [
+            "via" : "sms",
+            "country_code" : countryCode,
+            "phone_number" : phoneNumber
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: json, options: [])
+        } catch let error {
+            print(error)
+            request.httpBody = nil
+        }
+        
+        request.addValue(key, forHTTPHeaderField: "X-Authy-API-Key")
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            var success = false
+            defer {
+                DispatchQueue.main.async {
+                    completionHandler(success)
+                }
+            }
+            
+            guard error == nil else {
+                print("POST: Error requesting SMS: \(error)")
+                return
+            }
+            guard let data = data else { return }
+            let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+            
+            print(json)
+            
+            success = json["success"] as? Bool ?? false
+        }
+        task.resume()
+    }
+    
+    public func verify(countryCode: Int, phoneNumber: String, token: String, completionHandler: @escaping (Bool) -> Void) {
+        
+        let requestString = "https://api.authy.com/protected/json/phones/verification/check?phone_number=\(phoneNumber)&country_code=\(countryCode)&verification_code=\(token)"
+        
+        var request = URLRequest(url: URL(string: requestString)!)
+        request.httpMethod = "GET"
+        request.addValue(key, forHTTPHeaderField: "X-Authy-API-Key")
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            var success = false
+            defer {
+                DispatchQueue.main.async {
+                    completionHandler(success)
+                }
+            }
+            
+            guard error == nil else {
+                print("GET Error calling to verify token: \(error)")
+                return
+            }
+            guard let data = data else { return }
+            let json = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) as! [String:Any]
+            success = json["success"] as? Bool ?? success
+        }
+        task.resume()
+        
+    }
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import AuthyStoreTests
+
+XCTMain([
+     testCase(AuthyStoreTests.allTests),
+])
